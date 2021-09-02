bbc3daf5a4fb6958b950fe476771944fddc5ca75
jerryyxu <1193486073@qq.com>
2021-09-02 18:28:37 +0800

    'temp'

--- .github/actions/readme/dist/index.js
+++ .github/actions/readme/dist/index.js
@@ -6327,23 +6327,25 @@ const core = __nccwpck_require__(750);
 const github = __nccwpck_require__(903);
 
 const token = core.getInput('repo-token', { required: true });
+const sha = core.getInput('repo-sha', { required: true });
 const octokit = github.getOctokit(token);
 const { repository } = github.context.payload;
 const [owner, repo] = repository.full_name.split('/');
 
 const run = async () => {
   const issues = await octokit.rest.issues
-    .listForRepo({ owner, repo })
+    .listForRepo({ owner, repo, state: 'closed' })
     .then(({ data }) => data);
 
   console.log(issues);
 
   await octokit.rest.repos.createOrUpdateFileContents({
+    sha,
     owner,
     repo,
     path: 'README.md',
-    message: `post '${issues && issues.length && issues.map((v) => v.title)}'`,
-    content: 'ok',
+    message: 'update README.md',
+    content: issues && issues.length && issues.map((v) => v.title),
   });
 };
 
